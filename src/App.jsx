422217f1108bd38a267964bca09ddb81efe19652
Álvaro <alvarogutierrez884@gmail.com>
2025-07-19 15:25:08 -0300

    resize

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ import Menu from "./components/menu";
 function App() {
   return (
     <div className="grid grid-rows-[100px_1fr_100px]">
-      <div className="titulo">Merienda 19</div>
-      <Menu /> 
+      <div className="titulo flex w-full h-full items-center justify-center">
+        <div>Merienda</div>
+        <div className="numero">19</div>
+      </div>
+
+      <Menu />
     </div>
   );
 }
